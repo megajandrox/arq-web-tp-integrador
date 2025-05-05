f6fd2298a4c01f57a1cab7ea954a662f4882626c
megajandrox <megajandrox@gmail.com>
2025-05-05 19:22:42 -0300

    fxing settings.py

--- api/core/settings.py
+++ api/core/settings.py
@@ -5,8 +5,8 @@ import os
 load_dotenv()
 
 class Settings(BaseSettings):
-    DATABASE_URL: str = os.getenv("DATABASE_URL")
-    SECRET_KEY: str = os.getenv("SECRET_KEY")
+    DATABASE_URL: str = os.getenv("DATABASE_URL", "sqlite:///user_manager.db")
+    SECRET_KEY: str = os.getenv("SECRET_KEY", "your_secret_key")
     DEBUG: bool = os.getenv("DEBUG", False)
     ALLOWED_ORIGINS: str = os.getenv("ALLOWED_ORIGINS", "http://localhost:5173")
     API_PORT: int = int(os.getenv("API_PORT", 5000))
